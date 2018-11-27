f7c598d27dd9a94df6affbcaa245e47de61500f2
Revvick <mgs5email@gmail.com>
2018-11-27 13:02:34 -0500

    Just some changes

--- assignments/collection/jQuery/main.js
+++ assignments/collection/jQuery/main.js
@@ -1,14 +1,39 @@
 $(document).ready(function() {
     // run function on initial page load
-	demoFunction();
+	startFunction();
 });
 
 // mouse event link https://api.jquery.com/category/events/mouse-events/
-function demoFunction() {
-	
-	
-	
+function startFunction() {	
+
+	$(window).on('resize', function(){
+	if ( ($(window).height() < 1920) && ($(window).height() > 1023)  ){
+        $('.patternOne').addClass('patternOneToggle');
+        $('.patternTwo').addClass('patternTwoToggle');
+    }else{
+        $('.patternOne').removeClass('patternOneToggle');
+        $('.patternTwo').removeClass('patternTwoToggle');
+    }
+})
+
+
+
 	/*
+
+	 if ( (!$(this).hasClass("current")) && (!$("body").hasClass("home"))  ){
+    //...
+	
+	$(window).on('resize', function(){
+    if($(window).height() > 400) {
+        $('#body').addClass('limit1200');
+        $('#body').removeClass('limit400');
+    }else{
+        $('#body').addClass('limit400');
+        $('#body').removeClass('limit1200');
+    }
+	})
+	
+
 	$('.firstDiv').click(function() { //when i click this class
 		$('.testDiv').toggleClass('noBottomMargin'); //do something to this class
 	})
---
+++ assignments/collectionv2/jQuery/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    // run function on initial page load
+	demoFunction();
+});
+
+// mouse event link https://api.jquery.com/category/events/mouse-events/
+function demoFunction() {
+	
+	
+	
+	/*
+	$('.firstDiv').click(function() { //when i click this class
+		$('.testDiv').toggleClass('noBottomMargin'); //do something to this class
+	})
+
+	$('.secondDiv').hover(function() {
+		$('.maroon').toggleClass('notMaroon');
+	})
+
+	$('.thirdDiv').mouseenter(function() {
+		$('.testDiv').toggleClass('stable');
+	})
+	*/
+}
